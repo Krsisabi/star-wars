3935e6f1d0afdcaa46ac6783218c5703dbdd002a
Tim Yusupov <timboyus@gmail.com>
2023-10-29 21:29:37 +0300

    feat: add List component

---
+++ src/components/List/List.tsx
@@ -0,0 +1,17 @@
+import { Character } from '../../types';
+import { Card } from './Card';
+import styles from './List.module.scss';
+
+type ListProps = {
+  data: Character[];
+};
+
+export function List({ data }: ListProps) {
+  return (
+    <div className={styles.list}>
+      {data.map((el) => (
+        <Card key={el.name} {...el} />
+      ))}
+    </div>
+  );
+}
