ae4c5066bb3f9cda0b0df18712d8013d9549acb6
Tim Yusupov <timboyus@gmail.com>
2023-11-05 17:20:12 +0300

    chore: add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.tsx';
 import './index.scss';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary.tsx';
@@ -8,7 +10,9 @@ import { Error } from './components/Error';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<Error />}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
 );
