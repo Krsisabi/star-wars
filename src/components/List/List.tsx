3d99fb695ea899f532a505698b1b1319361c10af
Tim Yusupov <timboyus@gmail.com>
2023-11-25 14:05:22 +0200

    feat: add "nothing found" element

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -5,6 +5,9 @@ import styles from './List.module.scss';
 export function List() {
   const { results } = useSearchContext();
 
+  if (!results.length)
+    return <p className={styles.message}>Nothing found...</p>;
+
   return (
     <ul className={styles.list}>
       {results.map((el) => (
