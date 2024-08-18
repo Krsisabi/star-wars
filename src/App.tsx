eff6fff634b8a4ce773e27f52bfc32d3707c4f09
Tim Yusupov <timboyus@gmail.com>
2024-08-19 02:21:43 +0300

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import styles from './App.module.scss';
+import { Route, Routes } from 'react-router-dom';
+import { Home } from './components/Home';
+import { UncontrolledForm } from './components/UncontrolledForm';
+import { ControlledForm } from './components/ControlledForm';
+import { Error } from './components/Error';
 
-function App() {
-  return <div className={styles.app}></div>;
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/react-hook-form" element={<ControlledForm />} />
+      <Route path="/uncontrolled-form" element={<UncontrolledForm />} />
+      <Route path="/*" element={<Error />} />
+    </Routes>
+  );
+};
 
 export default App;
