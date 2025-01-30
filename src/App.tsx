ce3aa6767b383896f0c3926d395c5c055e83db36
Tim Yusupov <timboyus@gmail.com>
2025-01-30 21:52:12 +0300

    feat: add Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import styles from "./App.module.scss";
+import { Search } from './components/Search';
+import styles from './App.module.scss';
 
 function App() {
-  return <div className={styles.app}>test</div>;
+  return (
+    <div className={styles.app}>
+      <Search onChange={() => {}} onSubmit={() => {}} value="search form" />
+    </div>
+  );
 }
 
 export default App;
