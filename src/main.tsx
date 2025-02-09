552993155e3ddedadff211ffff3cc6d99e10d393
Tim Yusupov <timboyus@gmail.com>
2025-02-09 12:24:34 +0300

    chore: add redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import { ErrorBoundary } from './components/ErrorBoundary';
-import { Error } from './components/Error';
-import './index.scss';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import { Error } from './components/Error';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { store } from './store';
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary fallback={<Error />}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ErrorBoundary>
   </StrictMode>
