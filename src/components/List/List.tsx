fd53505cd854131badb9a276be1f423e0ff8701a
Tim Yusupov <timboyus@gmail.com>
2025-02-10 20:22:31 +0300

    feat: add checkboxes to list items

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,11 +1,13 @@
-import { Card } from './Card';
-import type { Character } from '~/types';
-import styles from './List.module.scss';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 import { SerializedError } from '@reduxjs/toolkit';
+import { useAppDispatch, useAppSelector } from '~/hooks/redux';
+import { toggleChecked } from '~/store/charactersSlice';
+import { CharacterNormilized } from '~/types';
+import { Card } from './Card';
+import styles from './List.module.scss';
 
 type ListProps = {
-  data?: Character[];
+  data?: CharacterNormilized[];
   activeElement?: string;
   setActiveElement?: React.Dispatch<React.SetStateAction<string>>;
   error?: FetchBaseQueryError | SerializedError;
@@ -17,7 +19,19 @@ export function List({
   setActiveElement,
   error,
 }: ListProps) {
+  const dispatch = useAppDispatch();
+  const selectedCharacters = useAppSelector(
+    (state) => state.selectedCharacters
+  );
+
   if (!data || !!error) return <div>Something went wrong</div>;
+
+  const isCharacterSelected = (id: number) =>
+    selectedCharacters.some((character) => character.id === id);
+
+  const onSelect = (character: CharacterNormilized) => {
+    dispatch(toggleChecked(character));
+  };
 
   return (
     <ul className={styles.list}>
@@ -25,9 +39,11 @@ export function List({
         return (
           <Card
             key={el.name}
-            {...el}
+            character={el}
             activeElement={activeElement}
             setActiveElement={setActiveElement}
+            isSelected={isCharacterSelected(el.id)}
+            onSelect={onSelect}
           />
         );
       })}
