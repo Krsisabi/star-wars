f2a49e5e528450874b17d65b32dca1b3dd4f5733
Tim Yusupov <timboyus@gmail.com>
2025-02-15 19:21:56 +0300

    chore: add test setup

--- tests/providers.tsx
+++ tests/providers.tsx
@@ -1,8 +1,14 @@
 import { PropsWithChildren } from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from '~/store';
 
 export const AllTheProviders = ({ children }: PropsWithChildren) => {
-  return <BrowserRouter>{children}</BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Provider store={store}>{children}</Provider>
+    </BrowserRouter>
+  );
 };
 
 AllTheProviders.displayName = 'AllTheProviders';
