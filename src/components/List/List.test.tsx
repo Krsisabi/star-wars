c655f495d64c41bf605c7b7dedb925865a9552fa
Tim Yusupov <timboyus@gmail.com>
2025-02-18 20:08:11 +0300

    test: add List component test

--- src/components/List/List.test.tsx
+++ src/components/List/List.test.tsx
@@ -10,4 +10,9 @@ describe('ListCards Component', () => {
     const cards = screen.getAllByRole('listitem');
     expect(cards).toHaveLength(mockData.length);
   });
+  it('renders "No such characters" message when data is an empty array', () => {
+    render(<List data={[]} />);
+
+    expect(screen.getByText(/no such/i)).toBeInTheDocument();
+  });
 });
