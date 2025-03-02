0fcf773362b656c38c8a7a81c412b5390996c38a
Tim Yusupov <timboyus@gmail.com>
2025-03-02 19:57:08 +0300

    feat: add last added form highlight

--- src/components/FormItem/FormItem.tsx
+++ src/components/FormItem/FormItem.tsx
@@ -3,10 +3,13 @@ import { FormValues } from '~/store/formReducer';
 
 type FormThumbnailProps = {
   form: FormValues;
+  isHighlighted: boolean;
 };
 
-const FormItem = ({ form }: FormThumbnailProps) => (
-  <li className={styles.formItem}>
+const FormItem = ({ form, isHighlighted }: FormThumbnailProps) => (
+  <li
+    className={`${styles.formItem} ${isHighlighted ? styles.highlighted : ''}`}
+  >
     <img className={styles.image} src={form.image} alt="image" />
     <div className={styles.text}>Type: {form.type}</div>
     <div className={styles.text}>Name: {form.name}</div>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,15 +2,35 @@ import styles from './Home.module.scss';
 import { useSelector } from 'react-redux';
 import { RootState } from '~/store';
 import FormItem from '../FormItem/FormItem';
+import { useEffect, useState } from 'react';
 
 export const Home = () => {
   const forms = useSelector((state: RootState) => state.form.forms);
+  const [highlightedId, setHighlightedId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (forms.length) {
+      const lastFormId = forms[forms.length - 1].id;
+      setHighlightedId(lastFormId);
+
+      const timer = setTimeout(() => {
+        setHighlightedId(null);
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [forms]);
+
   return (
     <section className={styles.wrapper}>
       {forms.length ? (
         <ul className={styles.list}>
           {forms.map((form) => (
-            <FormItem form={form} key={form.id} />
+            <FormItem
+              form={form}
+              key={form.id}
+              isHighlighted={form.id === highlightedId}
+            />
           ))}
         </ul>
       ) : (
